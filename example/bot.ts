1292b6a5c66481c979656fd896f57bcd81eebc7b
taskylizard <75871323+taskylizard@users.noreply.github.com>
2025-12-10 21:11:34 +0100

    chore: update example

--- example/bot.ts
+++ example/bot.ts
@@ -2,14 +2,14 @@ const ping = defineCommand({
   name: "ping",
   description: "Respond with pong",
   async run(ctx: MessageContext & { args: string[] }) {
-    await ctx.reply("pong from example");
+    let content = "Pong!\n"
+    content += `Args: ${ctx.args}\n`
+    await ctx.reply(content);
   },
 });
 
-on("messageCreate", (ctx) => {
-  console.log(ctx)
-})
+
 createBot({
-  prefix: "!",
+  prefix: ".",
   commands: [ping],
 });
